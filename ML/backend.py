de73a54b292b6852bcaee697e758170fca4fb5a6
Tharun-b777 <tharunkumaar7@gmail.com>
2021-01-17 12:59:24 +0530

    backend inital

---
+++ ML/backend.py
@@ -0,0 +1,34 @@
+from flask import Flask
+import joblib
+import librosa
+import glob
+import numpy as np
+import os
+app=Flask(__name__)
+model = joblib.load('Model')
+@app.route('/')
+def index():
+    try:
+        path = os.path.dirname(__file__) + '/../'
+        audioFiles = glob.glob(path+'*.wav')
+        message='Fake call'
+        for audioFile in audioFiles:
+
+            X, sample_rate = librosa.load(audioFile, res_type='kaiser_fast')
+
+            mfccs = np.asarray(np.mean(librosa.feature.mfcc(
+                y=X, sr=sample_rate, n_mfcc=40).T, axis=0))
+
+            predict = model.predict(mfccs.reshape(1, -1))
+            
+        if predict:
+            message='Emergecy'
+
+        return message
+
+        except:
+            return 'error'
+            
+
+if __name__ == '__main__':
+    app.run(debug=True)
